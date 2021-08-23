5900599e1f4da4c61401b9fa6807a2812e3e9756
roberto <roberto@Roberto-VLT01>
2021-08-23 16:12:04 +0200

    Update

--- src/main/resources/compileExternalClasses.sh
+++ src/main/resources/compileExternalClasses.sh
@@ -1,40 +1,39 @@
-::	This file is part of Burningwave Core.                                                                      
-::	                                                                                                            
-::	Author: Roberto Gentili                                                                                     
-::	                                                                                                            
-::	Hosted at: https://github.com/burningwave/jvm-driver                                                              
-::	                                                                                                            
-::	- -                                                                                                          
-::	                                                                                                            
-::	The MIT License (MIT)                                                                                       
-::	                                                                                                            
-::	Copyright (c) 2021 Roberto Gentili                                                                          
-::	                                                                                                            
-::	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-::	documentation files (the "Software"), to deal in the Software without restriction, including without        
-::	limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of   
-::	the Software, and to permit persons to whom the Software is furnished to do so, subject to the following    
-::	conditions:                                                                                                 
-::	                                                                                                            
-::	The above copyright notice and this permission notice shall be included in all copies or substantial        
-::	portions of the Software.                                                                                   
-::	                                                                                                            
-::	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT       
-::	LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO   
-::	EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
-::	AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
-::	OR OTHER DEALINGS IN THE SOFTWARE.
-::
-@echo off
-set JAVA_HOME=%1
-set RESOURCES_FOLDER=%2
+#	This file is part of Burningwave Core.                                                                      
+#	                                                                                                            
+#	Author: Roberto Gentili                                                                                     
+#	                                                                                                            
+#	Hosted at: https://github.com/burningwave/jvm-driver                                                              
+#	                                                                                                            
+#	- -                                                                                                          
+#	                                                                                                            
+#	The MIT License (MIT)                                                                                       
+#	                                                                                                            
+#	Copyright (c) 2021 Roberto Gentili                                                                          
+#	                                                                                                            
+#	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+#	documentation files (the "Software"), to deal in the Software without restriction, including without        
+#	limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of   
+#	the Software, and to permit persons to whom the Software is furnished to do so, subject to the following    
+#	conditions:                                                                                                 
+#	                                                                                                            
+#	The above copyright notice and this permission notice shall be included in all copies or substantial        
+#	portions of the Software.                                                                                   
+#	                                                                                                            
+#	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT       
+#	LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO   
+#	EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+#	AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+#	OR OTHER DEALINGS IN THE SOFTWARE.
+#
+#!bin/bash/
+echo off
+JAVA_HOME=$1
+RESOURCES_FOLDER=$2
 
-echo %RESOURCES_FOLDER%
+$JAVA_HOME/bin/javac -cp "$RESOURCES_FOLDER/../../../target/classes:$RESOURCES_FOLDER:" --release 9 "$RESOURCES_FOLDER/jdk/internal/loader/ClassLoaderDelegateForJDK9.java"
+$JAVA_HOME/bin/javac -cp "$RESOURCES_FOLDER/../../../target/classes:$RESOURCES_FOLDER:" --release 8 "$RESOURCES_FOLDER/java/lang/reflect/AccessibleSetterInvokerForJDK9.java"
+$JAVA_HOME/bin/javac -cp "$RESOURCES_FOLDER/../../../target/classes:$RESOURCES_FOLDER:" --release 8 "$RESOURCES_FOLDER/java/lang/ConsulterRetrieverForJDK9.java"
 
-call %JAVA_HOME%/bin/javac.exe -cp "%RESOURCES_FOLDER%/../../../target/classes;%RESOURCES_FOLDER%;" --release 9 "%RESOURCES_FOLDER%/jdk/internal/loader/ClassLoaderDelegateForJDK9.java"
-call %JAVA_HOME%/bin/javac.exe -cp "%RESOURCES_FOLDER%/../../../target/classes;%RESOURCES_FOLDER%;" --release 8 "%RESOURCES_FOLDER%/java/lang/reflect/AccessibleSetterInvokerForJDK9.java"
-call %JAVA_HOME%/bin/javac.exe -cp "%RESOURCES_FOLDER%/../../../target/classes;%RESOURCES_FOLDER%;" --release 8 "%RESOURCES_FOLDER%/java/lang/ConsulterRetrieverForJDK9.java"
-
-move "%RESOURCES_FOLDER%/jdk/internal/loader\ClassLoaderDelegateForJDK9.class" "%RESOURCES_FOLDER%/../../../target/classes/org/burningwave/jvm/driver/java/ClassLoaderDelegateForJDK9.bwc"
-move "%RESOURCES_FOLDER%/java/lang/reflect\AccessibleSetterInvokerForJDK9.class" "%RESOURCES_FOLDER%/../../../target/classes/org/burningwave/jvm/driver/java/AccessibleSetterInvokerForJDK9.bwc"
-move "%RESOURCES_FOLDER%/java/lang\ConsulterRetrieverForJDK9.class" "%RESOURCES_FOLDER%/../../../target/classes/org/burningwave/jvm/driver/java/ConsulterRetrieverForJDK9.bwc"
+mv "$RESOURCES_FOLDER/jdk/internal/loader/ClassLoaderDelegateForJDK9.class" "$RESOURCES_FOLDER/../../../target/classes/org/burningwave/jvm/driver/java/ClassLoaderDelegateForJDK9.bwc"
+mv "$RESOURCES_FOLDER/java/lang/reflect/AccessibleSetterInvokerForJDK9.class" "$RESOURCES_FOLDER/../../../target/classes/org/burningwave/jvm/driver/java/AccessibleSetterInvokerForJDK9.bwc"
+mv "$RESOURCES_FOLDER/java/lang/ConsulterRetrieverForJDK9.class" "$RESOURCES_FOLDER/../../../target/classes/org/burningwave/jvm/driver/java/ConsulterRetrieverForJDK9.bwc"
