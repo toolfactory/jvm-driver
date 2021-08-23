6ba6df8556e25adbddf06de7fb47d310ef012838
Burningwave <info@burningwave.org>
2021-08-23 20:55:59 +0200

    Update

--- src/main/resources/compileExternalClasses.sh
+++ src/main/resources/compileExternalClasses.sh
@@ -1,4 +1,3 @@
-#!bin/bash/
 #	This file is part of Burningwave JVM driver.                                                                      
 #	                                                                                                            
 #	Author: Roberto Gentili                                                                                     
@@ -26,6 +25,7 @@
 #	AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 #	OR OTHER DEALINGS IN THE SOFTWARE.
 #
+#!bin/bash/
 JAVA_HOME=$1
 RESOURCES_FOLDER=$2
 
