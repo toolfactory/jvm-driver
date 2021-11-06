29f35540f3a615a4b517b5c5d0c360c46c57e514
Roberto Gentili <roberto.gentili.1980@gmail.com>
2021-11-06 09:27:52 +0100

    Update

--- java/src/main/java/io/github/toolfactory/jvm/function/catalog/GetLoadedClassesRetrieverFunction.java
+++ java/src/main/java/io/github/toolfactory/jvm/function/catalog/GetLoadedClassesRetrieverFunction.java
@@ -205,7 +205,7 @@ public interface GetLoadedClassesRetrieverFunction extends Function<ClassLoader,
 				if (classLoader == null) {
 					throw new NullPointerException("Input classLoader parameter can't be null");
 				}
-				return new CleanableSupplier<>() {
+				return new CleanableSupplier<Collection<Class<?>>>() {
 					Collection<Class<?>> classes;
 
 					@Override
