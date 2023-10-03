b4351bbc6b9b9602e4660056974b228e19e84d49
Roberto Gentili <roberto.gentili.1980@gmail.com>
2023-10-03 12:13:43 +0200

    Added JDK 21 support

--- java/src/main/java/io/github/toolfactory/jvm/Info.java
+++ java/src/main/java/io/github/toolfactory/jvm/Info.java
@@ -31,7 +31,7 @@
 package io.github.toolfactory.jvm;
 
 public interface Info {
-	public final static int[] CRITICAL_VERSIONS = {7, 9, 14, 17, 20};
+	public final static int[] CRITICAL_VERSIONS = {7, 9, 14, 17, 20, 21};
 
 
 	public boolean isCompressedOopsOffOn64BitHotspot();
